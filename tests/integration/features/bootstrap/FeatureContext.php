6eeb33d3c352e450627bc794b53731a016d3c8a2
Joas Schilling <nickvergessen@owncloud.com>
2015-11-18 12:01:13 +0100

    Inherit FeatureContext of core

--- tests/integration/features/bootstrap/FeatureContext.php
+++ tests/integration/features/bootstrap/FeatureContext.php
@@ -4,12 +4,8 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Behat\Tester\Exception\PendingException;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
 use GuzzleHttp\Client;
 use GuzzleHttp\Message\ResponseInterface;
-
 
 /**
  * Defines application features from the specific context.
@@ -18,18 +14,6 @@ class FeatureContext implements Context, SnippetAcceptingContext {
 
 	/** @var string */
 	private $baseUrl = '';
-
-	/** @var ResponseInterface */
-	private $response = null;
-
-	/** @var int */
-	private $apiVersion = 1;
-
-	/** @var string[]  */
-	private $adminUser = ['admin', 'admin'];
-
-	/** @var array */
-	private $createdUsers = [];
 
 	/**
 	 * Initializes context.
@@ -46,112 +30,14 @@ class FeatureContext implements Context, SnippetAcceptingContext {
 		}
 	}
 
-	/**
-	 * @Given using api version :arg1
-	 */
-	public function usingApiVersion($version) {
-		$this->apiVersion = $version;
-	}
+	/** @var \Behat\Behat\Context\Context */
+	private $coreContext;
 
-	/**
-	 * @Given user :arg1 exists
-	 */
-	public function userExists($user) {
-		try {
-			$this->doesUserExist($user);
-		} catch (\GuzzleHttp\Exception\ClientException $ex) {
-			$this->creatingTheUser($user);
-		}
-		$this->doesUserExist($user);
-		PHPUnit_Framework_Assert::assertEquals(200, $this->response->getStatusCode());
-	}
-
-	/**
-	 * @When /^sending "([^"]*)" to "([^"]*)" with$/
-	 * @param \Behat\Gherkin\Node\TableNode|null $formData
-	 */
-	public function sendingToWith($verb, $url, $body) {
-		$fullUrl = $this->baseUrl . "v{$this->apiVersion}.php" . $url;
-		$client = new Client();
-		$options = [];
-		$options['auth'] = $this->adminUser;
-		if ($body instanceof \Behat\Gherkin\Node\TableNode) {
-			$fd = $body->getRowsHash();
-			$options['body'] = $fd;
-		}
-
-		try {
-			$this->response = $client->send($client->createRequest($verb, $fullUrl, $options));
-		} catch (\GuzzleHttp\Exception\ClientException $ex) {
-			$this->response = $ex->getResponse();
-		}
-	}
-
-	/**
-	 * @Then /^the OCS status code should be "([^"]*)"$/
-	 */
-	public function theOCSStatusCodeShouldBe($statusCode) {
-		PHPUnit_Framework_Assert::assertEquals($statusCode, $this->getOCSResponse($this->response));
-	}
-
-	/**
-	 * @Then /^the HTTP status code should be "([^"]*)"$/
-	 */
-	public function theHTTPStatusCodeShouldBe($statusCode) {
-		PHPUnit_Framework_Assert::assertEquals($statusCode, $this->response->getStatusCode());
-	}
-
-	/**
-	 * Parses the xml answer to get ocs response which doesn't match with
-	 * http one in v1 of the api.
-	 */
-	private function getOCSResponse($response) {
-		return $response->xml()->meta[0]->statuscode;
-	}
-
-	private function creatingTheUser($user) {
-		$fullUrl = $this->baseUrl . "v{$this->apiVersion}.php/cloud/users";
-		$client = new Client();
-		$options = [];
-		$options['auth'] = $this->adminUser;
-
-		$options['body'] = [
-			'userid' => $user,
-			'password' => '123456'
-		];
-
-		$this->response = $client->send($client->createRequest("POST", $fullUrl, $options));
-		$this->createdUsers[$user] = $user;
-	}
-
-	public function doesUserExist($user){
-		$fullUrl = $this->baseUrl . "v2.php/cloud/users/$user";
-		$client = new Client();
-		$options = [];
-		$options['auth'] = $this->adminUser;
-
-		$this->response = $client->get($fullUrl, $options);
-	}
-
-	public function deleteUser($user) {
-		$fullUrl = $this->baseUrl . "v{$this->apiVersion}.php/cloud/users/$user";
-		$client = new Client();
-		$options = [];
-		$options['auth'] = $this->adminUser;
-
-		$this->response = $client->send($client->createRequest("DELETE", $fullUrl, $options));
-	}
-
-
-	/**
-	 * @BeforeScenario
-	 * @AfterScenario
-	 */
-	public function cleanupUsers()
+	/** @BeforeScenario */
+	public function gatherContexts(\Behat\Behat\Hook\Scope\BeforeScenarioScope $scope)
 	{
-		foreach($this->createdUsers as $user) {
-			$this->deleteUser($user);
-		}
-	}
+		$environment = $scope->getEnvironment();
 
+		$this->coreContext = $environment->getContext('CoreContext');
+	}
 }
--- tests/integration/features/notifications.feature
+++ tests/integration/features/notifications.feature
@@ -2,10 +2,20 @@ Feature: notifications
   Background:
     Given using api version "1"
 
-  Scenario: Creating a new notification
-    Given user "user0" exists
-    When sending "POST" to "/apps/notifications/v1/notifications" with
-      | subject | test |
+  Scenario: Create test user
+    Given As an "admin"
+    And user "test1" does not exist
+    When sending "POST" to "/cloud/users" with
+      | userid | test1 |
+      | password | 123456 |
     Then the OCS status code should be "100"
     And the HTTP status code should be "200"
+    And user "test1" exists
+
+  Scenario: Creating a new notification
+    Given As an "test1"
+    When sending "GET" to "/notifications/v1"
+    Then the HTTP status code should be "200"
+    # 204 No Content - Because there is no notifier
+    And the OCS status code should be "204"
 
