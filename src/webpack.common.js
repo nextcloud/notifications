e22661f67b38553cd9ac0f6c57c5cc0ecf25c2b8
Joas Schilling <coding@schilljs.com>
2018-10-23 14:06:07 +0200

    Remove unrequired change
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/webpack.common.js
+++ src/webpack.common.js
@@ -42,8 +42,7 @@ module.exports = {
 	plugins: [new VueLoaderPlugin()],
 	resolve: {
 		alias: {
-			vue$: 'vue/dist/vue.esm.js',
-			'handlebars': 'handlebars/runtime.js'
+			vue$: 'vue/dist/vue.esm.js'
 		},
 		extensions: ['*', '.js', '.vue', '.json']
 	}
