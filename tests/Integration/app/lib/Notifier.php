66637374c1fe099c17e1fe160cb9465a113d0c19
Joas Schilling <coding@schilljs.com>
2019-04-30 14:26:26 +0200

    Fix integration test dummy implementation
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/Integration/app/lib/Notifier.php
+++ tests/Integration/app/lib/Notifier.php
@@ -28,12 +28,32 @@ use OCP\Notification\INotifier;
 class Notifier implements INotifier {
 
 	/**
+	 * Identifier of the notifier, only use [a-z0-9_]
+	 *
+	 * @return string
+	 * @since 17.0.0
+	 */
+	public function getID(): string {
+		return 'notificationsintegrationtesting';
+	}
+
+	/**
+	 * Human readable name describing the notifier
+	 *
+	 * @return string
+	 * @since 17.0.0
+	 */
+	public function getName(): string {
+		return 'Integration test';
+	}
+
+	/**
 	 * @param INotification $notification
 	 * @param string $languageCode The code of the language that should be used to prepare the notification
 	 * @return INotification
 	 * @throws \InvalidArgumentException When the notification was not prepared by a notifier
 	 */
-	public function prepare(INotification $notification, $languageCode) {
+	public function prepare(INotification $notification, string $languageCode): INotification {
 		if ($notification->getApp() === 'notificationsintegrationtesting') {
 			$notification->setParsedSubject($notification->getSubject());
 			$notification->setParsedMessage($notification->getMessage());
