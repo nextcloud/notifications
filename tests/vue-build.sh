3fa7093c37c5c2bdcbc57b29ef2ceb61d61e41df
Joas Schilling <coding@schilljs.com>
2019-07-18 10:03:49 +0200

    Adjust the vue check to the newest way
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/vue-build.sh
+++ tests/vue-build.sh
@@ -1,36 +1,9 @@
 #!/bin/bash
+set -e
 
-root=$(pwd)
-entryFile=$1
+# Build the Vue files
+make build-js-production
 
-if [ ! -f "$entryFile" ]
-then
-	echo "The build file $entryFile does not exists"
-	exit 2
-else
-	backupFile="$entryFile.orig"
-	path=$(dirname "$entryFile")
+git status
 
-	# Backup original file
-	echo "Backing up $entryFile to $backupFile"
-	cp $entryFile $backupFile
-
-	# Make the app
-	set -e
-	cd "$path/../"
-	make
-
-	# Reset
-	cd $root
-
-	# Compare build files
-	echo "Comparing $entryFile to the original"
-	if ! diff -q $entryFile $backupFile &>/dev/null
-	then
-		echo "$entryFile build is NOT up-to-date! Please send the proper production build within the pull request"
-		cat $HOME/.npm/_logs/*.log
-		exit 2
-	else
-		echo "$entryFile build is up-to-date"
-	fi
-fi
+bash -c "[[ ! \"`git status --porcelain js`\" ]] || ( echo 'Uncommitted changes in built Vue files' && exit 1 )"
