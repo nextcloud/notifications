ca2582f4a322576f0a48d1734e991e0c1dad1d52
Joas Schilling <coding@schilljs.com>
2020-09-10 09:36:57 +0200

    Simplify webpack config
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- webpack.js
+++ webpack.js
@@ -1,35 +1,13 @@
-const { merge } = require('webpack-merge')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 const BabelLoaderExcludeNodeModulesExcept = require('babel-loader-exclude-node-modules-except')
 
-const config = {
-	module: {
-		rules: [
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-				exclude: BabelLoaderExcludeNodeModulesExcept([
-					'@juliushaertl/vue-richtext',
-					'vue-material-design-icons',
-				]),
-			},
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: BabelLoaderExcludeNodeModulesExcept([
-					'@juliushaertl/vue-richtext',
-					'@nextcloud/event-bus',
-					'semver',
-				]),
-			},
-		],
-	},
-}
+const jsRule = webpackConfig.module.rules.findIndex(t => (t.test.toString() === /\.js$/.toString()))
 
-const mergedConfigs = merge(config, webpackConfig)
-
-// Remove duplicate rules by the `test` key
-mergedConfigs.module.rules = mergedConfigs.module.rules.filter((v, i, a) => a.findIndex(t => (t.test.toString() === v.test.toString())) === i)
+webpackConfig.module.rules[jsRule].exclude = BabelLoaderExcludeNodeModulesExcept([
+	'@juliushaertl/vue-richtext',
+	'@nextcloud/event-bus',
+	'semver',
+])
 
 // Merge rules by replacing existing tests
-module.exports = mergedConfigs
+module.exports = webpackConfig
