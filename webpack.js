90fe389d1ae69789f375c876c3681a102bac4e50
Joas Schilling <coding@schilljs.com>
2021-01-05 13:34:18 +0100

    Exclude some more libraries
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- webpack.js
+++ webpack.js
@@ -6,8 +6,17 @@ const jsRule = webpackConfig.module.rules.findIndex(t => (t.test.toString() === 
 webpackConfig.module.rules[jsRule].exclude = BabelLoaderExcludeNodeModulesExcept([
 	'@juliushaertl/vue-richtext',
 	'@nextcloud/event-bus',
+	'is-absolute-url',
+	'is-plain-obj',
 	'semver',
 ])
+webpackConfig.module.rules.push({
+	test: /\.mjs$/,
+	loader: 'babel-loader',
+	exclude: BabelLoaderExcludeNodeModulesExcept([
+		'rehype-add-classes',
+	]),
+})
 
 // Merge rules by replacing existing tests
 module.exports = webpackConfig
