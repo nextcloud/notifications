3c42355e7d386d6f686ec3acf013a1ce0cda39b1
Joas Schilling <coding@schilljs.com>
2021-01-28 09:18:50 +0100

    Try starting the server in the action
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/Integration/run.sh
+++ tests/Integration/run.sh
@@ -6,9 +6,9 @@ APP_INTEGRATION_DIR=$PWD
 ROOT_DIR=${APP_INTEGRATION_DIR}/../../../..
 composer install
 
-php -S localhost:8080 -t ${ROOT_DIR} &
-PHPPID=$!
-echo $PHPPID
+#php -S localhost:8080 -t ${ROOT_DIR} &
+#PHPPID=$!
+#echo $PHPPID
 
 cp -R ./app ../../../notificationsintegrationtesting
 ${ROOT_DIR}/occ app:enable notifications
@@ -21,7 +21,7 @@ export TEST_SERVER_URL="http://localhost:8080/"
 ${APP_INTEGRATION_DIR}/vendor/bin/behat -f junit -f pretty $1 $2
 RESULT=$?
 
-kill $PHPPID
+#kill $PHPPID
 
 ${ROOT_DIR}/occ app:disable notificationsintegrationtesting
 rm -rf ../../../notificationsintegrationtesting
