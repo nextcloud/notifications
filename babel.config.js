ac70fa986cc3cf06644177fa5f0c39606cdda1c9
Vincent Petry <vincent@nextcloud.com>
2021-03-26 12:09:09 +0100

    Move babel/env config into babelrc file
    Signed-off-by: Vincent Petry <vincent@nextcloud.com>
    Signed-off-by: npmbuildbot-nextcloud[bot] <npmbuildbot-nextcloud[bot]@users.noreply.github.com>

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,13 @@
 module.exports = {
+	plugins: ['add-module-exports'],
 	presets: [
 		[
 			'@babel/preset-env',
 			{
 				corejs: 3,
 				useBuiltIns: 'entry',
+				modules: 'commonjs',
 			},
 		],
 	],
 }
-
