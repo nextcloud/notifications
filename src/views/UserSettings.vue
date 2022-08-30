6c97fd7651de65b518bdf42dc0ec3f325bbf7c3e
Joas Schilling <coding@schilljs.com>
2022-08-30 20:50:49 +0200

    Fix duplicate ID on settings page
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- src/views/UserSettings.vue
+++ src/views/UserSettings.vue
@@ -25,11 +25,12 @@
 				<strong v-if="!config.is_email_set">{{ t('notifications', 'You need to set up your email address before you can receive notification emails.') }}</strong>
 			</div>
 			<p>
-				<label for="notify_setting_batchtime" class="notification-frequency__label">
+				<label for="notification_reminder_batchtime" class="notification-frequency__label">
 					{{ t('notifications', 'Send email reminders about unhandled notifications after:') }}
 				</label>
 				<select
-					id="notify_setting_batchtime"
+					id="notification_reminder_batchtime"
+					name="notification_reminder_batchtime"
 					v-model="config.setting_batchtime"
 					class="notification-frequency__select"
 					@change="updateSettings()">
