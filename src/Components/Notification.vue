e9e24dd0b2bf979cba12c48305ac2d80c2e0ff5b
Joas Schilling <coding@schilljs.com>
2022-09-20 08:55:41 +0200

    Fix checking for links finally
    Signed-off-by: Joas Schilling <coding@schilljs.com>
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- src/Components/Notification.vue
+++ src/Components/Notification.vue
@@ -185,7 +185,7 @@ export default {
 
 			let parametersHaveLink = false
 			Object.keys(this.subjectRichParameters).forEach(p => {
-				if (p.link) {
+				if (this.subjectRichParameters[p].link) {
 					parametersHaveLink = true
 				}
 			})
