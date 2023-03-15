8a3464fea1a183e595a908c8cb972792ae45fa05
Joas Schilling <coding@schilljs.com>
2023-03-15 19:37:39 +0100

    feat(notifications): Expose an attribute whether there is a desktop client handling it
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ lib/Service/ClientService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace OCA\Notifications\Service;
+
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\Diagnostics\IQuery;
+use OCP\IDBConnection;
+use OCP\IRequest;
+
+class ClientService {
+	public const DESKTOP_CLIENT_TIMEOUT = 120;
+
+	public function __construct(
+		protected IDBConnection $connection,
+		protected IRequest $request,
+	) {
+	}
+
+	public function hasTalkDesktop(string $userId, int $maxAge = 0): bool {
+		$query = $this->connection->getQueryBuilder();
+		$query->select('name')
+			->from('authtoken')
+			->where($query->expr()->eq('uid', $query->createNamedParameter($userId)));
+
+		if ($maxAge !== 0) {
+			$query->andWhere($query->expr()->gte(
+				'last_activity',
+				$query->createNamedParameter($maxAge, IQueryBuilder::PARAM_INT)
+			));
+		}
+
+		$result = $query->executeQuery();
+		while ($row = $result->fetch()) {
+			\OC::$server->getLogger()->error(json_encode($row['name']));
+			if (preg_match('/ \(Talk Desktop Client - [A-Za-z ]+\)$/', $row['name'])) {
+				$result->closeCursor();
+				return true;
+			}
+		}
+		$result->closeCursor();
+
+		return false;
+	}
+}
