24db45b8baca05a5cc1b369674137b169c1fbced
Joas Schilling <coding@schilljs.com>
2023-06-21 09:54:17 +0200

    fix(settings): Delete settings when a user is deleted
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Listener/UserDeletedListener.php
+++ lib/Listener/UserDeletedListener.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 /**
  * @copyright Copyright (c) 2020 Daniel Kesselberg <mail@danielkesselberg.de>
  *
+ * @author Joas Schilling <coding@schilljs.com>
  * @author Daniel Kesselberg <mail@danielkesselberg.de>
  *
  * @license GNU AGPL version 3 or any later version
@@ -27,6 +28,7 @@ declare(strict_types=1);
 namespace OCA\Notifications\Listener;
 
 use OCA\Notifications\Handler;
+use OCA\Notifications\Model\SettingsMapper;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
 use OCP\User\Events\UserDeletedEvent;
@@ -35,9 +37,15 @@ class UserDeletedListener implements IEventListener {
 
 	/** @var Handler */
 	private $handler;
+	/** @var SettingsMapper */
+	private $settingsMapper;
 
-	public function __construct(Handler $handler) {
+	public function __construct(
+		Handler $handler,
+		SettingsMapper $settingsMapper,
+	) {
 		$this->handler = $handler;
+		$this->settingsMapper = $settingsMapper;
 	}
 
 	public function handle(Event $event): void {
@@ -48,5 +56,6 @@ class UserDeletedListener implements IEventListener {
 
 		$user = $event->getUser();
 		$this->handler->deleteByUser($user->getUID());
+		$this->settingsMapper->deleteSettingsByUser($user->getUID());
 	}
 }
