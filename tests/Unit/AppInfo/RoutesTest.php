2d7ec85dc875101588bea4eb95c076ba4124bd67
Joas Schilling <coding@schilljs.com>
2024-01-24 10:17:02 +0100

    feat(push): Try the new Route attribute
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/Unit/AppInfo/RoutesTest.php
+++ tests/Unit/AppInfo/RoutesTest.php
@@ -37,6 +37,6 @@ class RoutesTest extends TestCase {
 		$this->assertCount(1, $routes);
 		$this->assertArrayHasKey('ocs', $routes);
 		$this->assertIsArray($routes['ocs']);
-		$this->assertCount(10, $routes['ocs']);
+		$this->assertCount(8, $routes['ocs']);
 	}
 }
