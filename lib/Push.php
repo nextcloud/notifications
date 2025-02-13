e310cb1de05f6d539ba17a780dc13691a7437c5c
Marcel Müller <marcel-mueller@gmx.de>
2025-02-13 16:37:13 +0100

    feat(push): Show error when internet connection is disabled in configuration
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- lib/Push.php
+++ lib/Push.php
@@ -197,6 +197,8 @@ class Push {
 
 	public function pushToDevice(int $id, INotification $notification, ?OutputInterface $output = null): void {
 		if (!$this->config->getSystemValueBool('has_internet_connection', true)) {
+			$this->printInfo('<error>Internet connectivity is disabled in configuration file - no push notifications will be sent</error>');
+
 			return;
 		}
 
