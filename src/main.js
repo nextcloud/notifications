2c0fba475e800e70ae91e928eb96eaa73cebb7be
Grigorii K. Shartsev <me@shgk.me>
2025-04-25 14:04:46 +0200

    fix: hotfix to prevent double mount on production
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/main.js
+++ src/main.js
@@ -14,12 +14,19 @@ Vue.prototype.n = n
 Vue.prototype.OC = OC
 Vue.prototype.OCA = OCA
 
-export default new Vue({
-	el: '#notifications',
-	// eslint-disable-next-line vue/match-component-file-name
-	name: 'NotificationsApp',
-	components: {
-		NotificationsApp: () => import(/* webpackPreload: true */'./NotificationsApp.vue'),
-	},
-	render: h => h('NotificationsApp'),
-})
+// Hotfix to prevent creating 2 apps on production
+// See: https://github.com/nextcloud/notifications/issues/2164
+if (!window._nc_notifications_app_initialized) {
+	window._nc_notifications_app_initialized = true
+
+	// eslint-disable-next-line no-new
+	new Vue({
+		el: '#notifications',
+		// eslint-disable-next-line vue/match-component-file-name
+		name: 'NotificationsApp',
+		components: {
+			NotificationsApp: () => import('./NotificationsApp.vue'),
+		},
+		render: h => h('NotificationsApp'),
+	})
+}
