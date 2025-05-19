6e9ad1dd66228d5541ce8adb327a2f7da711c73c
Côme Chilliet <come.chilliet@nextcloud.com>
2025-05-19 18:03:26 +0200

    chore(tests): Cleanup bootstrap.php to be forward-compatible
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -1,19 +1,23 @@
 <?php
+
+declare(strict_types=1);
+
 /**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
  * SPDX-FileCopyrightText: 2016 ownCloud, Inc.
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+
+use OCP\App\IAppManager;
+use OCP\Server;
 
 if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
 
 require_once __DIR__ . '/../../../../lib/base.php';
+require_once __DIR__ . '/../../../../tests/autoload.php';
 
-// Fix for "Autoload path not allowed: .../tests/lib/testcase.php"
-\OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
-
-// Fix for "Autoload path not allowed: .../notifications/tests/testcase.php"
-\OC_App::loadApp('notifications');
+Server::get(IAppManager::class)->loadApp('notifications');
 
 OC_Hook::clear();
