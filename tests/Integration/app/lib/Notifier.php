dc58f80dceb5af5cf2d7f9ef16b9a77e6dd60095
Joas Schilling <coding@schilljs.com>
2025-06-03 17:28:35 +0200

    test: Remove debug logs triggered by the testing app
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/Integration/app/lib/Notifier.php
+++ tests/Integration/app/lib/Notifier.php
@@ -1,5 +1,8 @@
 <?php
+
+declare(strict_types=1);
 /**
+ * SPDX-FileCopyrightText: 2016-2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-FileCopyrightText: 2016 ownCloud, Inc.
  * SPDX-License-Identifier: AGPL-3.0-only
  */
@@ -8,34 +11,17 @@ namespace OCA\NotificationsIntegrationTesting;
 
 use OCP\Notification\INotification;
 use OCP\Notification\INotifier;
+use OCP\Notification\UnknownNotificationException;
 
 class Notifier implements INotifier {
-	/**
-	 * Identifier of the notifier, only use [a-z0-9_]
-	 *
-	 * @return string
-	 * @since 17.0.0
-	 */
 	public function getID(): string {
 		return 'notificationsintegrationtesting';
 	}
 
-	/**
-	 * Human readable name describing the notifier
-	 *
-	 * @return string
-	 * @since 17.0.0
-	 */
 	public function getName(): string {
 		return 'Integration test';
 	}
 
-	/**
-	 * @param INotification $notification
-	 * @param string $languageCode The code of the language that should be used to prepare the notification
-	 * @return INotification
-	 * @throws \InvalidArgumentException When the notification was not prepared by a notifier
-	 */
 	public function prepare(INotification $notification, string $languageCode): INotification {
 		if ($notification->getApp() === 'notificationsintegrationtesting') {
 			$notification->setParsedSubject($notification->getSubject());
@@ -43,6 +29,6 @@ class Notifier implements INotifier {
 			return $notification;
 		}
 
-		throw new \InvalidArgumentException();
+		throw new UnknownNotificationException();
 	}
 }
