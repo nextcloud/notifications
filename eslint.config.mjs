d63d5d41cd6a0d244f90f2cee4f5bd4cbe1e33b5
Maksim Sukharev <antreesy.web@gmail.com>
2025-06-11 10:41:17 +0200

    chore(eslint): update config
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import { recommendedVue2Javascript } from '@nextcloud/eslint-config'
+
+export default [
+	...recommendedVue2Javascript,
+	// Disabled rules from recommendedVue2Javascript pack
+	{
+		rules: {
+			'@nextcloud/vue/no-deprecated-exports': 'off',
+			'@nextcloud/vue/no-deprecated-props': 'off',
+			'@stylistic/array-bracket-newline': 'off',
+			'@stylistic/arrow-parens': 'off',
+			'@stylistic/indent': 'off',
+			'@stylistic/max-statements-per-line': 'off',
+			'antfu/top-level-function': 'off',
+			'jsdoc/tag-lines': 'off',
+			'no-console': 'off',
+			'no-unused-vars': 'off',
+			'no-use-before-define': 'off',
+			'perfectionist/sort-imports': 'off',
+			'perfectionist/sort-named-imports': 'off',
+			'vue/define-macros-order': 'off',
+			'vue/first-attribute-linebreak': 'off',
+			'vue/multi-word-component-names': 'off',
+			'vue/new-line-between-multi-line-property': 'off',
+			'vue/no-boolean-default': 'off',
+			'vue/no-required-prop-with-default': 'off',
+			'vue/no-unused-properties': 'off',
+			'vue/no-unused-refs': 'off',
+		},
+	},
+]
