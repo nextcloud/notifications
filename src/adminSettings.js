87c052657a1cb708c8d871314b3b2f02b12ee374
Maksim Sukharev <antreesy.web@gmail.com>
2025-06-17 17:56:13 +0200

    chore(vue3): update initializing Vue apps
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/adminSettings.js
+++ src/adminSettings.js
@@ -3,16 +3,10 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import Vue from 'vue'
+import { createApp } from 'vue'
 import AdminSettings from './views/AdminSettings.vue'
 
 // Styles
 import '@nextcloud/dialogs/style.css'
 
-Vue.prototype.t = t
-Vue.prototype.n = n
-
-export default new Vue({
-	el: '#notifications-admin-settings',
-	render: (h) => h(AdminSettings),
-})
+export default createApp(AdminSettings).mount('#notifications-admin-settings')
