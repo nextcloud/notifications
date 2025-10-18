bb0554d8722a8f7151f2dad35e7995f470509e27
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-10-18 12:35:31 +0000

    chore(assets): Recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/BrowserStorage-FmX8gBxO.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{L as o}from"./index-_0iU0BYK-CjtyBpO5.chunk.mjs";const i=o("notifications").clearOnLogout().persist().build();export{i as B};
-//# sourceMappingURL=BrowserStorage-FmX8gBxO.chunk.mjs.map
