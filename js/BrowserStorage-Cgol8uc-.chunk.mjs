c0c383aca699193d9fdc00148c01a42085f5f331
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-11-09 15:01:14 +0000

    chore(assets): Recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/BrowserStorage-Cgol8uc-.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{M as o}from"./index-hZPKu-D6-BRDHbonb.chunk.mjs";const i=o("notifications").clearOnLogout().persist().build();export{i as B};
-//# sourceMappingURL=BrowserStorage-Cgol8uc-.chunk.mjs.map
---
+++ js/BrowserStorage-DU5JcXR7.chunk.mjs
@@ -0,0 +1,2 @@
+import{M as o}from"./index-hZPKu-D6-CerkQMDj.chunk.mjs";const i=o("notifications").clearOnLogout().persist().build();export{i as B};
+//# sourceMappingURL=BrowserStorage-DU5JcXR7.chunk.mjs.map
