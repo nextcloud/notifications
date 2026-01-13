221625d051f8cf3607357a5a94c250d80c2f2f6e
sim <git@sgougeon.fr>
2026-01-13 16:41:17 +0100

    feat(webpush): Fix continues after token validation
    Signed-off-by: sim <git@sgougeon.fr>

--- lib/Push.php
+++ lib/Push.php
@@ -335,7 +335,7 @@ class Push {
 					$this->deleteWebPushToken($device['token']);
 					// no break
 				case TokenValidation::OLD:
-					continue;
+					continue 2;
 			}
 
 			// If the endpoint got a 429 TOO_MANY_REQUESTS,
@@ -412,7 +412,7 @@ class Push {
 					$this->deleteProxyPushToken($device['token']);
 					// no break
 				case TokenValidation::OLD:
-					continue;
+					continue 2;
 			}
 
 			try {
@@ -528,7 +528,7 @@ class Push {
 					$this->deleteWebPushToken($device['token']);
 					// no break
 				case TokenValidation::OLD:
-					continue;
+					continue 2;
 			}
 
 			// If the endpoint got a 429 TOO_MANY_REQUESTS,
@@ -601,7 +601,7 @@ class Push {
 					$this->deleteProxyPushToken($device['token']);
 					// no break
 				case TokenValidation::OLD:
-					continue;
+					continue 2;
 			}
 
 			try {
