b3e20927555a855bacc1a97f3ec4b509010843b7
Joas Schilling <coding@schilljs.com>
2026-03-27 09:26:19 +0100

    chore(assets): Recompile assets
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ js/BrowserStorage-DBNZzeXR.chunk.mjs
@@ -0,0 +1,2 @@
+import{ah as o}from"./_plugin-vue_export-helper-uhMn_HPi.chunk.mjs";const i=o("notifications").clearOnLogout().persist().build();export{i as B};
+//# sourceMappingURL=BrowserStorage-DBNZzeXR.chunk.mjs.map
