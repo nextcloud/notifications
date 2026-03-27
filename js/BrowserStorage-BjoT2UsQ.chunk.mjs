f918a0c946bf3c52e62edca15c9cefcdcc4da7a6
Joas Schilling <coding@schilljs.com>
2026-03-27 14:04:08 +0100

    chore(assets): Recompile assets
    Signed-off-by: Joas Schilling <coding@schilljs.com>
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ js/BrowserStorage-BjoT2UsQ.chunk.mjs
@@ -0,0 +1,2 @@
+import{ag as o}from"./_plugin-vue_export-helper-DmSfxBjQ.chunk.mjs";const i=o("notifications").clearOnLogout().persist().build();export{i as B};
+//# sourceMappingURL=BrowserStorage-BjoT2UsQ.chunk.mjs.map
