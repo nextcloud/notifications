54e00b4a0182f354c747e0a0876e2b39af2010b4
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-06-15 07:23:59 +0000

    fix(deps): Fix npm audit
    Signed-off-by: GitHub <noreply@github.com>

--- js/BrowserStorage-nEOdRpj-.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{a9 as o}from"./_plugin-vue_export-helper-BqvTAhh3.chunk.mjs";const i=o("notifications").clearOnLogout().persist().build();export{i as B};
-//# sourceMappingURL=BrowserStorage-nEOdRpj-.chunk.mjs.map
